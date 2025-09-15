fe73608991f3e46f6387fa4c280599ce0e025775
DavorSkokandic <davor.skokandic46@gmail.com>
2025-09-15 19:00:58 +0200

    Updated start script in package.json to build before running the server, ensuring the latest code is executed.

---
+++ src/config/cleanup.config.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.cleanupConfig = void 0;
+exports.cleanupConfig = {
+    // Maximum age of files in hours before they get deleted (default: 72 hours = 3 days)
+    maxAgeHours: process.env.FILE_MAX_AGE_HOURS ? parseInt(process.env.FILE_MAX_AGE_HOURS) : 72,
+    // How often to run cleanup in minutes
+    intervalMinutes: process.env.CLEANUP_INTERVAL_MINUTES ? parseInt(process.env.CLEANUP_INTERVAL_MINUTES) : 60,
+    // Upload directory path
+    uploadDir: process.env.UPLOAD_DIR || 'uploads',
+    // Enable/disable automatic cleanup
+    enabled: process.env.AUTO_CLEANUP_ENABLED !== 'false', // defaults to true unless explicitly disabled
+    // Log cleanup operations
+    logOperations: process.env.LOG_CLEANUP !== 'false' // defaults to true unless explicitly disabled
+};
+exports.default = exports.cleanupConfig;
